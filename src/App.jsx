873272564aefe0a30c0aa245ac173f480cd2e4c8
AntonRusev <anton_r@abv.bg>
2025-03-30 15:55:24 +0300

    added the app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import styles from "./App.module.css";
+
 function App() {
-  return (<div>Chatbot</div>);
+  return (
+    <div className={styles.App}>
+      <header className={styles.Header}>
+        {/* Image */}
+        <img
+          className={styles.Logo}
+          src="/chat-bot.png"
+          alt="bot"
+        />
+        {/* Title */}
+        <h2 className={styles.Title}>Chatbot</h2>
+      </header>
+
+      {/* Chat container */}
+      <div className={styles.ChatContainer} />
+    </div>
+  );
 }
 
 export default App;
